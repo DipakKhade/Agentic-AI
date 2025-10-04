1cbaa965ae95fd78931b917c387ecf5c5168028f
Dipak Khade <dipakhade214@gmail.com>
2025-10-04 22:36:06 +0530

    basics

---
+++ 02_datatypes/numbers.py
@@ -0,0 +1,17 @@
+
+# itergers
+# decimal
+# complex
+# real , imaginary
+# bools
+
+from decimal import Decimal
+from fractions import Fraction
+
+print(3 + True)
+
+if bool('dipak'):
+    print('name is valid')
+
+
+print(1 - 0.99999999999)
---
+++ 02_datatypes/string.py
@@ -0,0 +1,4 @@
+
+name = 'Dipak'
+print(name[1:])
+print(name[::-1])
