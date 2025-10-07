3624cd2567c9e2c112b451c3d28aae1de6532b41
Dipak Khade <dipakhade214@gmail.com>
2025-10-07 18:18:42 +0530

    checkpoint

--- 02_revisit/opps.py
+++ 02_revisit/opps.py
@@ -14,3 +14,36 @@ class UserManager:
 new_user = UserManager(user_id=1, user_name='Dipak')
 print(new_user.get_user())
 
+
+class AdminManager(UserManager):
+    def __init__(self, user_id, user_name):
+        super().__init__(user_id, user_name)
+    
+
+new_admin = AdminManager(2, 'Gaurav')
+print(new_admin.get_user())
+
+
+class A:
+    def __init__(self):
+        pass
+
+    def a_method(self):
+        print('printing from class A')
+
+class B:
+    a_instane = A
+
+    def __init__(self):
+        self.b = self.a_instane()
+    
+    def b_method(self):
+        self.b.a_method()
+
+b = B()
+b.b_method()
+
+
+class C(A, B):
+    def __init__(self):
+        super().__init__()
