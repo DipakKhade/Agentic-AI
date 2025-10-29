4755355d033e6a4cf62fa25153951d85f42cfca8
Dipak Khade <dipakhade214@gmail.com>
2025-10-29 11:04:46 +0530

    added node

--- langgraph/main.py
+++ langgraph/main.py
@@ -9,3 +9,10 @@ class MessagesState(TypedDict):
     llm_calls: int
 
 graph_builder = StateGraph(MessagesState)
+
+def chatBot(state: MessagesState) -> MessagesState :
+    #do llm call
+    return {"messae": "this is a static msg retured from chat bot node"}
+
+graph_builder.add_node("chatBot", chatBot)
+
