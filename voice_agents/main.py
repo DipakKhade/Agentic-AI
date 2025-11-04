6fb8716f8911c4ced3c13959145e1cb39ef46130
Dipak Khade <dipakhade214@gmail.com>
2025-11-04 17:53:20 +0530

    checkpoint

--- voice_agents/main.py
+++ voice_agents/main.py
@@ -1,4 +1,17 @@
 import speech_recognition as sr
+from openai import OpenAI
+from system_prompt import SYSTEM_PROMPT
+from dotenv import load_dotenv
+import pyttsx3
+
+
+load_dotenv()
+
+openai  = OpenAI(
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+engine = pyttsx3.init()
 
 def main():
     voice_recognizer = sr.Recognizer()
@@ -12,6 +25,19 @@ def main():
         text_from_audio = voice_recognizer.recognize_google(audio)
 
         print('text_from_audio ---', text_from_audio)
+
+        response = openai.chat.completions.create(
+            model="gemini-2.5-flash",
+            messages=[
+                {"role":"system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": text_from_audio}
+            ]
+        )
+
+        print(response.choices[0].message.content)
+
+        engine.say(response.choices[0].message.content)
+        engine.runAndWait()
         
 if __name__ == "__main__":
     main()
