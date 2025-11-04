ebe9d2b4c2ad1d436e61ba0455d8b4e6b46ab0a9
Dipak Khade <dipakhade214@gmail.com>
2025-11-04 16:51:16 +0530

    listning for audio

--- voice_agents/main.py
+++ voice_agents/main.py
@@ -1,7 +1,13 @@
 import speech_recognition as sr
 
 def main():
-    recognizer = sr.Recognizer()
+    voice_recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        voice_recognizer.adjust_for_ambient_noise(source)
+        voice_recognizer.pause_threshold(2)
+
+        audio = voice_recognizer.listen(source=source)
 
 
 if __name__ == "__main__":
